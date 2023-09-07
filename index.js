b37ea059cb27014ee2a09a3e63537606b576ef0e
hisaya-i <igon0505@gmail.com>
2023-09-07 12:38:06 +0900

    docker-compose

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+host: 'db',
+user: 'root',
+password: 'password',
+database: 'sample'
+});
+const hostname = '0.0.0.0';
+const port = 3000;
+const server = http.createServer((req, res) => {
+connection.query(
+'SELECT id FROM `users`',
+function(err, results, fields) {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/plain');
+results.forEach((row) => {
+res.write(`id: ${row['id']}\n`);
+});
+res.end();
+}
+);
+});
+server.listen(port, hostname, () => {
+console.log(`Server running at http://${hostname}:${port}/`);
+});
